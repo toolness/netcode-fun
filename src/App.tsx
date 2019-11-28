039e67798ad0cb9a72508bcd6668c6f66dc16b7d
Atul Varma <varmaa@gmail.com>
2019-11-28 14:29:42 -0500

    Origination.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-viewports">
+        <div>
+          <h2>Player 1</h2>
+        </div>
+        <div>
+          <h2>Server</h2>
+        </div>
+        <div>
+          <h2>Player 2</h2>
+        </div>
+      </div>
     </div>
   );
 }
