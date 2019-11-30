b2c9c63b66bd782fed345b8b6e9ff4a735b2e9fa
Atul Varma <varmaa@gmail.com>
2019-11-30 08:33:58 -0500

    Attempt to use usePrevious.

--- src/Vec2Input.tsx
+++ src/Vec2Input.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState, useRef } from 'react';
 import { Button, InputContext } from './InputManager';
 import { Vec2 } from './Vec2';
 
@@ -9,6 +9,17 @@ function getAxis(positive: boolean, negative: boolean): number {
   return -1.0;
 }
 
+// https://stackoverflow.com/a/53446665
+function usePrevious<T>(value: T): T|undefined {
+  const ref = useRef<T|undefined>(undefined);
+
+  useEffect(() => {
+    ref.current = value;
+  });
+
+  return ref.current;
+}
+
 export const Vec2Input: React.FC<{
   up: Button,
   down: Button,
@@ -16,17 +27,21 @@ export const Vec2Input: React.FC<{
   right: Button,
   onChange: (velocity: Vec2) => void
 }> = (props) => {
-  const {onChange} = props;
+  const { onChange } = props;
   const {
     [props.up]: up,
     [props.down]: down,
   } = useContext(InputContext);
+
   const [yVel, setYVel] = useState(getAxis(!!down, !!up));
+  const prevYVel = usePrevious(yVel);
 
   useEffect(() => setYVel(getAxis(!!down, !!up)), [up, down]);
   useEffect(() => {
-    onChange({x: 0.0, y: yVel});
-  }, [yVel, onChange]);
+    if (yVel !== prevYVel) {
+      onChange({x: 0.0, y: yVel});
+    }
+  }, [yVel, prevYVel, onChange]);
 
   return null;  
 };
