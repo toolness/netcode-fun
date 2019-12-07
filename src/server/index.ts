1f08cea4f34c2881b05b78f99fe512f9f62f7978
Atul Varma <varmaa@gmail.com>
2019-12-07 18:41:27 -0500

    Actually, let's not use Twilio/WebRTC for now.

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,19 +1,15 @@
 import WebSocket from 'ws';
 import dotenv from 'dotenv';
-import twilio from 'twilio';
-import { getPositiveIntEnv, getRequiredEnv } from './env';
+import { getPositiveIntEnv } from './env';
 
 dotenv.config({path: '.env.local'});
 
 const PORT = getPositiveIntEnv('PORT', '3001');
-const TWILIO_ACCOUNT_SID = getRequiredEnv('TWILIO_ACCOUNT_SID');
-const TWILIO_AUTH_TOKEN = getRequiredEnv('TWILIO_AUTH_TOKEN');
 
 export function run() {
   const wss = new WebSocket.Server({ port: PORT }, () => {
     console.log(`WebSocket server listening on port ${PORT}.`);
   });
-  const twilioClient = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
   wss.on('connection', ws => {
     console.log('connection!');
