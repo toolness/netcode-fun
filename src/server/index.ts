2f3b45103ce8626af6e267241aabc3496e6aa1c3
Atul Varma <varmaa@gmail.com>
2019-12-07 17:53:49 -0500

    Move twilio client to module scope.

--- src/server/index.ts
+++ src/server/index.ts
@@ -10,14 +10,13 @@ const TWILIO_ACCOUNT_SID = getRequiredEnv('TWILIO_ACCOUNT_SID');
 const TWILIO_AUTH_TOKEN = getRequiredEnv('TWILIO_AUTH_TOKEN');
 
 const app = express();
+const twilioClient = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
 app.get('/', (req, res) => {
   res.send("Hallo");
 });
 
 export function run() {
-  const twilioClient = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
-
   app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}.`);
   });
